e8118f79c7d94e28d0d888437a5de0b5c76489dc
kwin <wonkung@163.com>
2023-01-11 20:35:30 +0800

    fix: read password

--- util/flags.go
+++ util/flags.go
@@ -1,17 +1,14 @@
 package util
 
 import (
-	"errors"
+	"bufio"
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"strings"
-	"syscall"
 	"toprelayer/config"
 
 	"github.com/urfave/cli/v2"
-	"golang.org/x/crypto/ssh/terminal"
 )
 
 var (
@@ -53,42 +50,54 @@ func MakePassword(ctx *cli.Context, cfg *config.Config) (string, error) {
 	return lines[0], err
 }
 
+//func ReadPassword(cfg *config.Config) (string, error) {
+//	fmt.Print(">>> Please Enter " + cfg.RelayerToRun + " pasword:\n>>> ")
+//
+//	var passwd string
+//	if terminal.IsTerminal(syscall.Stdin) {
+//		pass, err := terminal.ReadPassword(syscall.Stdin)
+//		if err != nil {
+//			return string(pass), err
+//		}
+//		passwd = string(pass)
+//	} else {
+//		var b [1]byte
+//		var pw []byte
+//		for {
+//			n, err := os.Stdin.Read(b[:])
+//			// terminal.ReadPassword discards any '\r', so we do the same
+//			if n > 0 && b[0] != '\r' {
+//				if b[0] == '\n' {
+//					return string(pw), nil
+//				}
+//				pw = append(pw, b[0])
+//				// limit size, so that a wrong input won't fill up the memory
+//				if len(pw) > 1024 {
+//					err = errors.New("password too long")
+//				}
+//			}
+//			if err != nil {
+//				// terminal.ReadPassword accepts EOF-terminated passwords
+//				// if non-empty, so we do the same
+//				if err == io.EOF && len(pw) > 0 {
+//					err = nil
+//				}
+//				return string(pw), err
+//			}
+//		}
+//	}
+//	fmt.Println()
+//	return passwd, nil
+//}
+
 func ReadPassword(cfg *config.Config) (string, error) {
 	fmt.Print(">>> Please Enter " + cfg.RelayerToRun + " pasword:\n>>> ")
-
-	var passwd string
-	if terminal.IsTerminal(syscall.Stdin) {
-		pass, err := terminal.ReadPassword(syscall.Stdin)
-		if err != nil {
-			return string(pass), err
-		}
-		passwd = string(pass)
-	} else {
-		var b [1]byte
-		var pw []byte
-		for {
-			n, err := os.Stdin.Read(b[:])
-			// terminal.ReadPassword discards any '\r', so we do the same
-			if n > 0 && b[0] != '\r' {
-				if b[0] == '\n' {
-					return string(pw), nil
-				}
-				pw = append(pw, b[0])
-				// limit size, so that a wrong input won't fill up the memory
-				if len(pw) > 1024 {
-					err = errors.New("password too long")
-				}
-			}
-			if err != nil {
-				// terminal.ReadPassword accepts EOF-terminated passwords
-				// if non-empty, so we do the same
-				if err == io.EOF && len(pw) > 0 {
-					err = nil
-				}
-				return string(pw), err
-			}
-		}
+	line, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		return "", err
 	}
-	fmt.Println()
-	return passwd, nil
+	if len(line) == 0 {
+		return "", fmt.Errorf("input is empty")
+	}
+	return line, nil
 }
